a67ad81e23169536254cc4284cb7c6783efc7052
Елизавета <110974646+KETLiz@users.noreply.github.com>
2023-07-06 13:34:07 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,11 @@
+import java.util.PriorityQueue;
+
 public class Main
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 	    
 		Console c = new Console();
-		c.addToy(new Toy(1, "li", 35));
-		c.addToy(new Toy(2, "Vi", 80));
-		c.addToyByUser();
-		
-		c.showToys();
-		c.chooseToy();
-		System.out.println(c.showChooseToys());
+		c.start();
 	}
 }
+
